d22613028ea6d7883c834d752d403d60b71b74b4
Keith Doyle <kdoyle@seic.com>
2023-09-14 09:31:00 -0400

    create the header section and menu items

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
+import Header from "@/components/Header";
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "IMDB Clone",
@@ -12,7 +10,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        {/* Header */}
+        <Header />
+
+        {/* Navbar */}
+
+        {/* SearchBox */}
+
+        {children}
+      </body>
     </html>
   );
 }
