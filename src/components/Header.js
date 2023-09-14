f3ee24aa458d25d7a8b1bb2435aa64117cda2283
Keith Doyle <kdoyle@seic.com>
2023-09-14 13:31:10 -0400

    add dark/light mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import MenuItem from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import DarkModeSwitch from "./DarkModeSwitch";
 
 export default function Header() {
   return (
@@ -10,10 +11,13 @@ export default function Header() {
         <MenuItem title="HOME" address="/" Icon={AiFillHome} />
         <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
-      <div>
+      <div className="flex items-center space-x-5">
+        <DarkModeSwitch />
         <Link href="/">
           <h2 className="text-2xl">
-            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">IMDB</span>
+            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
+              IMDB
+            </span>
             <span className="text-sl hidden sm:inline">Clone</span>
           </h2>
         </Link>
